656d14d4ba14961153c7f5e6683cc76b07a73974
marius herlea <mariusherlea@gmail.com>
2026-01-10 08:22:56 +0100

    live e live, dev e dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,13 @@ export default async function ArticlesPage(props: {
     <div className="max-w-6xl mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-8 text-center">Toate articolele</h1>
 
+<div className="relative z-50">
+  <Link href={`/articles/${articles.slug}`}>
+    TEST LINK
+  </Link>
+</div>
+
+
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {articles.map((article: any) => (
           <Link
